2f5597aa3fe68fa8bcac44bcb748dd0247908271
Muhammad Dian Nafi <dian.nafi@staff.uinsaid.ac.id>
2024-07-24 08:15:34 +0700

    update db profile user

--- database/migrations/2022_12_13_143924_create_profile_users_table.php
+++ database/migrations/2022_12_13_143924_create_profile_users_table.php
@@ -22,13 +22,60 @@ class CreateProfileUsersTable extends Migration
                 ->onUpdate('cascade')->onDelete('cascade');
             $table->string('nama');
             $table->string('username');
+            $table->string('prodi')->nullable();
             $table->string('email')->nullable();
+            $table->string('semester')->nullable();
             $table->string('foto')->nullable();
             $table->string('tempat_lahir')->nullable();
             $table->date('tanggal_lahir')->nullable();
             $table->string('gender')->nullable();
+            $table->string('agama')->nullable();
             $table->string('no_hp')->nullable();
             $table->string('alamat')->nullable();
+           
+            $table->string('provinsi_id')->nullable();
+            // $table->foreign('provinsi_id')
+            //     ->references('id')
+            //     ->on('provinces')
+            //     ->onUpdate('cascade')->onDelete('cascade');
+
+            $table->string('kabupaten_id')->nullable();
+            // $table->foreign('kabupaten_id')
+            //     ->references('id')
+            //     ->on('regencies')
+            //     ->onUpdate('cascade')->onDelete('cascade');
+
+            $table->string('kecamatan_id')->nullable();
+            // $table->foreign('kecamatan_id')
+            //     ->references('id')
+            //     ->on('districts')
+            //     ->onUpdate('cascade')->onDelete('cascade');
+
+            $table->string('kelurahan_id')->nullable();
+            // $table->foreign('kelurahan_id')
+            //     ->references('id')
+            //     ->on('villages')
+            //     ->onUpdate('cascade')->onDelete('cascade');
+
+             //data orang tua
+             $table->string('nama_ayah')->nullable();
+             $table->string('nama_ibu')->nullable();
+             $table->string('pekerjaan_ayah')->nullable();
+             $table->string('pekerjaan_ibu')->nullable();
+             $table->string('pendidikan_ayah')->nullable();
+             $table->string('pendidikan_ibu')->nullable();
+             $table->string('nohp_ayah')->nullable();
+             $table->string('nohp_ibu')->nullable();
+ 
+            // $table->string('berkas_ortu');//kk akte ijazah raport penghasilan
+
+            //data asal sekolah
+            // $table->string('sekolah_sd')->nullable();
+            // $table->string('sekolah_smp')->nullable();
+            $table->string('sekolah_sma')->nullable();
+            $table->string('berkas_siswa');//kk akte ijazah raport
+            $table->string('prestasi')->nullable();
+
             $table->timestamps();
         });
     }
