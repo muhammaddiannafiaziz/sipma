02cf81ef4c8784a8070cd94d1f85b80ab31b9113
Muhammad Dian Nafi <dian.nafi@staff.uinsaid.ac.id>
2024-07-27 16:39:40 +0700

    tambah informasi alur pendaftaran

--- resources/views/dashboard/dashboard-user.blade.php
+++ resources/views/dashboard/dashboard-user.blade.php
@@ -8,7 +8,7 @@
     </ol>
 </div>
 <div class="row">
-    <div class="col-xl-12">
+    <div class="col-xl-8">
         <div class="card transparent-card">
             <div class="bootstrap-carousel">
                 <div id="carouselExampleIndicators2" class="carousel slide" data-bs-ride="carousel">
@@ -46,7 +46,38 @@
             </div>
         </div>
     </div>
-    
 </div>
+<div class="row page-titles" style="border-radius: 0%">
+    <p>Alur Pendaftaran "PENERIMAAN MAHASANTRI BARU"</p>
+    <p><strong>Ma'had Al-Jami'ah (Pesantren Mahasiswa) Ronggowarsito</strong></p>
+    <p>UIN Raden Mas Said Surakarta</p>
+    <p>Tahun Akademik 2024/2025</p>
+    <hr>
+    <p><strong>Berkas yang harus dipersiapkan</strong></p>
+    <p>1.<span style="font-family: Times New Roman;">&nbsp;&nbsp;&nbsp;</span>Screenshot data NIM di menu Cek UKT <strong><a href="https://registrasimaba.uinsaid.ac.id">registrasimaba.uinsaid.ac.id</a></strong></p>
+    <p>2.<span style="font-family: Times New Roman;">&nbsp;&nbsp;&nbsp;</span>Scan Bukti Pembayaran UKT</p>
+    <p>3.<span style="font-family: Times New Roman;">&nbsp;&nbsp;&nbsp;</span>Pakta Integritas&nbsp;(dapat diunduh di <strong><a href="https://bit.ly/PMBMahadAlJamiahUINSAID2024">bit.ly/PMBMahadAlJamiahUINSAID2024</a></strong>)</p>
+    <p>4.<span style="font-family: Times New Roman;">&nbsp;&nbsp;&nbsp;</span>Scan Sertifikat / Piagam Prestasi (bila ada)</p>
+    <p>5.<span style="font-family: Times New Roman;">&nbsp;&nbsp;&nbsp;</span>Foto Formal</p>
+    <p>keterangan : untuk berkas pendaftaran nomor 1 sampai 4 dijadikan satu file pdf</p>
+    <p>&nbsp;</p>
+    <p><strong>Langkah Pendaftaran</strong></p>
+    <p>1.<span style="font-family: Times New Roman;">&nbsp;&nbsp;&nbsp;</span>Kunjungi laman <strong><a href="https://mahad.uinsaid.ac.id/sipma/">mahad.uinsaid.ac.id/sipma</a></strong></p>
+    <p>2.<span style="font-family: Times New Roman;">&nbsp;&nbsp;&nbsp;</span>Klik 'DAFTAR' untuk membuat akun pendaftaran. Isikan sesuai dengan identitas.</p>
+    <p>3.<span style="font-family: Times New Roman;">&nbsp;&nbsp;&nbsp;</span>Setelah berhasil melakukan pembuatan akun, anda akan berada di halaman beranda.</p>
+    <p>4.<span style="font-family: Times New Roman;">&nbsp;&nbsp;&nbsp;</span>Klik menu / foto profil untuk melengkapi data akun. Pada isian upload berkas pendaftaran silakan unggah file berkas yang telah dipersiapkan.</p>
+    <p>5.<span style="font-family: Times New Roman;">&nbsp;&nbsp;&nbsp;</span>Setelah data akun lengkap, klik menu pendaftaran untuk melakukan pendaftaran.</p>
+    <p>6.<span style="font-family: Times New Roman;">&nbsp;&nbsp;&nbsp;</span>Klik 'Daftar Seleksi'. Kemudian anda akan diminta mengunggah foto formal. Lalu klik 'Buat Pendaftaran'.</p>
+    <p>7.<span style="font-family: Times New Roman;">&nbsp;&nbsp;&nbsp;</span>Setelah melakukan pendaftaran, mohon untuk menunggu sebentar hingga status pendaftaran 'Terverifikasi'.</p>
+    <p>8.<span style="font-family: Times New Roman;">&nbsp;&nbsp;&nbsp;</span>Jika status pendaftaran sudah terverifikasi, link 'Grup Whatsapp' akan muncul dan silakan masuk grup tersebut.</p>
+    <p>9.<span style="font-family: Times New Roman;">&nbsp;&nbsp;&nbsp;</span>Ujian seleksi akan diinformasikan di grup whatsapp.</p>
+    <p>10.Setelah mengikuti seleksi, tahap selanjutnya adalah pengumuman hasil seleksi.</p>
+    <p>11.Hasil Seleksi akan diumumkan di laman <strong><a href="https://mahad.uinsaid.ac.id/sipma/">mahad.uinsaid.ac.id/sipma</a></strong> pada menu 'pengumuman'.</p>
+    <p>12.Peserta yang lolos dapat melakukan pembayaran kafa'ah ma'hadiyah (biaya ma'had).</p>
+    <p>13.Setelah melakukan pembayaran, unggah scan bukti pembayaran di menu 'pengumuman'.</p>
+    <p>14.Langkah terakhir adalah mencetak 'Pengumuman Pendaftaran Mahaantri Baru' dalam ukuran A4 untuk melalukan daftar ulang ketika masuk ma'had.</p>
+    <hr>
+    <p>Contact Person : <strong><a href="https://wa.me/62818780801">+62 818 780 801 (Muhammad Dian Nafi)</a></strong></p>
 
+</div>
 
